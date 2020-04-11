4f411f000503f4213b97e0563a53d6bfbdff3276
yanlibo2021 <yanlibo2021@126.com>
2020-04-11 22:36:15 +0800

    modified:   package.json         modified:   vue.config.js         modified:   yarn.lock

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 
 const path = require('path');
-
+const fs = require('fs')
+const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin')
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
@@ -29,8 +30,34 @@ module.exports = {
                 symbolId: 'icon-[name]'
             })
             .end()
+
+        config.plugins.delete('preload')
+        config.plugins.delete('prefetch') // 移除 prefetch 插件
     },
-    configureWebpack: config => {
-        console.log(' configureWebpack: config => {');
-    }
+    //configureWebpack: config => {
+    //     // 动态添加第三方打包好的组件
+    //     // 引入 node.js 核心库，获取第三方库文件夹下的文件名数组
+    //     const files = fs.readdirSync(path.resolve(__dirname, './dll'));
+    //     // 遍历数组，根据指定的条件，生成插件的第三方库加载代码。
+    //     files.forEach(file => {
+    //         if (/.*\.dll.js$/.test(file)) {
+    //             // // 添加指定的文件，到 html 文件中去。
+    //             config.plugins.push(new AddAssetHtmlWebpackPlugin({
+    //                 filepath: path.resolve(__dirname, './dll', file),
+    //                 // 文件输出目录
+    //                 outputPath: 'lib',
+    //                 // 脚本或链接标记的公共路径
+    //                 publicPath: 'lib'
+    //             }))
+
+    //         }
+    //         if (/.*\.manifest.json/.test(file)) {
+    //             // 这个插件避免 webpack 重复打包第三方库，如果要打包第三方库，会先到这个映射文件中去找，已经打包好的。
+    //             plugins.push(new webpack.DllReferencePlugin({
+    //                 manifest: path.resolve(__dirname, './dll', file)
+    //             }))
+    //         }
+    //     })
+
+    // }
 }
