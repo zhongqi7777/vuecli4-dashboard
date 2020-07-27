507dfed7ba5ce85793493b5e14d569e35f7433bf
zhongqi7777 <yanlibo@300.cn>
2020-07-27 16:51:54 +0800

    modified:   package-lock.json         modified:   package.json         new file:   src/assets/image/mall/shop/default.png         new file:   src/assets/image/mall/shop/default@2x.png         new file:   src/assets/image/mall/title/default.png         new file:   src/assets/image/mall/title/default@2x.png         modified:   src/router/index.js         new file:   src/views/mall/index.vue

--- src/router/index.js
+++ src/router/index.js
@@ -21,10 +21,15 @@ export const constantRoutes = [
     component: () => import("@/views/Login/index")
   },
   {
+    path: "/mall",
+    name: "mall",
+    component: () => import("@/views/mall/index")
+  },
+  {
     path: "",
     // component: Container,
     component: () => import("@/container/index"),
-    redirect: "login",
+    redirect: "mall",
     children: [
       {
         path: "dashboard",
---
+++ src/views/mall/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mall">
+    <el-row class="head">
+      <el-col :span="24">
+        <el-row>
+          <el-col :span="4">
+            <div class="left">
+              <img src="../../assets/image/mall/shop/default.png" alt="" />
+              <span>店铺名称</span>
+            </div></el-col
+          >
+          <el-col :span="16" class="content"><div>交易数据大屏</div></el-col>
+          <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "mall",
+};
+</script>
+
+<style lang="scss">
+.mall {
+  min-height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background: #000b23;
+
+  .head {
+    background: url("../../assets/image/mall/title/default.png") no-repeat;
+    background-size: cover;
+    -webkit-background-size: cover;
+    -o-background-size: cover;
+    background-position: center 0;
+    height: 84px;
+    .left {
+      margin: 15px 0 0 35px;
+      display: flex;
+      align-items: center;
+      span {
+        font-size: 14px;
+        font-weight: 500;
+        color: rgba(255, 255, 255, 0.8);
+        margin-left: 2px;
+      }
+    }
+    .content {
+      font-size: 42px;
+      color: rgba(255, 255, 255, 1);
+      text-align: center;
+      padding: 17px 0;
+    }
+  }
+}
+</style>
