9091d7110ea66f8b9444236b2ce261bbfe865079
zhongqi7777 <yanlibo@300.cn>
2020-07-28 11:54:15 +0800

    modified:   src/views/mall/index.vue

--- src/views/mall/index.vue
+++ src/views/mall/index.vue
@@ -37,7 +37,20 @@
         <div class="grid-content bg-purple"></div>
       </el-col>
       <el-col :span="10">
-        <div class="grid-content middle-grid-margin"></div>
+        <div class="grid-content middle-grid-margin">
+          <el-row type="flex" justify="space-between" class="grid-content-row1">
+            <el-col :span="12"
+              ><div class="grid-content-row1-col1-div">
+                <span>今日交易金额</span>
+              </div></el-col
+            >
+            <el-col :span="12"
+              ><div class="grid-content-row1-col2-div">
+                <span>同比昨日 <span>增长20%</span></span>
+              </div></el-col
+            >
+          </el-row>
+        </div>
       </el-col>
       <el-col :span="7">
         <div class="grid-content bg-purple"></div>
@@ -139,8 +152,8 @@ export default {
     margin: 20px 35px;
   }
 
-
-  .grid-content,.row-3-grid-content,
+  .grid-content,
+  .row-3-grid-content,
   .row-4-grid-content {
     height: 220px;
     background: rgba(71, 225, 255, 0.02);
@@ -150,11 +163,48 @@ export default {
 
   .grid-content {
     height: 430px;
-    width: 100%;
+    //width: 100%;
+    .grid-content-row1-col1-div,
+    .grid-content-row1-col2-div {
+      height: 40px;
+      display: flex;
+      align-items: center;
+    }
+
+    .grid-content-row1 {
+      margin: 8px;
+    }
+
+    .grid-content-row1-col1-div {
+      height: 35px;
+      background: linear-gradient(
+        90deg,
+        rgba(71, 225, 255, 0.3) 0%,
+        rgba(71, 225, 255, 0) 100%
+      );
+      span {
+        font-size: 16px;
+        font-weight: 500;
+        color: rgba(255, 255, 255, 1);
+        margin: 0 10px;
+      }
+    }
+
+    .grid-content-row1-col2-div {
+      justify-content: flex-end;
+      span {
+        font-size: 14px;
+        font-weight: 400;
+        color: rgba(255, 255, 255, 1);
+        span{
+          color: #FFE14B;
+        }
+      }
+    }
   }
 
-  .middle-grid-margin{
-    margin: 0 15px;
+  .middle-grid-margin {
+    margin: 0 10px;
   }
 }
 </style>
