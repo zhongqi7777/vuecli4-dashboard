938e327f2ccd36346cd7cf18443a379f2eca0e64
zhongqi7777 <yanlibo@300.cn>
2020-08-17 11:30:44 +0800

    modified:   src/views/mall/components/pay/index.vue

--- src/views/mall/components/pay/index.vue
+++ src/views/mall/components/pay/index.vue
@@ -1,21 +1,11 @@
 <template>
-  <!-- <div class="pay">
-    <el-row class="row" v-for="(item, index) in payData" :key="index">
-      <el-col
-        :span="8"
-        v-for="(subItem, subindex) in item.children"
-        :key="subindex"
-      >
-        <div class="grid-box">
+  <div class="wrap">
+    <div class="item-row" v-for="(item, index) in payData" :key="index">
+      <div class="item-col"     v-for="(subItem, subindex) in item.children"
+        :key="subindex">
           <vitem :title="subItem.title" :content="subItem.content"></vitem>
         </div>
-      </el-col>
-    </el-row>
-  </div> -->
-
-  <div class="wrap" data-style="display: flex; flex-direction: column;">
-    <div class="item" data-style="flex-grow: 1">a1; flex-grow: 1</div>
-    <div class="item" data-style="flex-grow: 1">a2; flex-grow: 1</div>
+    </div>
   </div>
 </template>
 
@@ -73,63 +63,28 @@ export default {
 
 <style lang="scss">
 .wrap {
-  background-color: #73a75c;
-  border: 1px solid #3a542f;
-  height: 50%;
-
-  .item {
-    background-color: #8fb97c;
-    border: 1px solid #c7ddbd;
-    padding: 10px;
-    height: 100%;
-  }
-}
-
-.grid-content-text {
-  background: red;
   height: 100%;
-  width: 300%;
-
-  .grid-content-item {
-    background: yellow;
-    height: 100%;
-    width: 100%;
-  }
-}
-
-.pay {
-  height: 100%;
-  // display: flex;
-  // flex-direction: column;
-
   display: flex;
-  // display: -webkit-box; /* OLD - iOS 6-, Safari 3.1-6 */
-  // display: -moz-box; /* OLD - Firefox 19- (buggy but mostly works) */
-  // display: -ms-flexbox; /* TWEENER - IE 10 */
-  // display: -webkit-flex; /* NEW - Chrome */
-
   flex-direction: column;
-  // flex-direction: -ms-column;
 
-  .row {
-    //flex: 1;
-    flex: 1;
-    -ms-flex: 1;
-    -webkit-flex: 1;
-
+  .item-row {
+    height: 100%;
     margin: 10px 0 0 10px;
-
-    .grid-box {
+    display: flex;
+    flex-direction: row;
+    .item-col {
+      flex: 1;
       background: RGBA(4, 25, 50, 1);
       height: 100%;
       margin-right: 10px;
       cursor: pointer;
     }
 
-    .grid-box:hover {
+    .item-col:hover {
       background: rgba(71, 225, 255, 0.05);
       border: 1px solid rgba(6, 201, 221, 1);
     }
   }
 }
+
 </style>
