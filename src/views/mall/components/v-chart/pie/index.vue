28fb3f5e9bd9d38394dde58f8711ad5b83884c46
zhongqi7777 <yanlibo@300.cn>
2020-08-18 09:57:25 +0800

    new file:   src/views/mall/components/v-chart/pie/README.md         new file:   src/views/mall/components/v-chart/pie/index.vue         modified:   src/views/mall/modules/PayMallDeal/index.vue

---
+++ src/views/mall/components/v-chart/pie/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="panel-content">
+    <!-- <div style="width: 300px;height: 300px;background-color: #0E1824;" ref="chartContent">
+        </div> -->
+
+    <div style="width: 150px;height: 150px;" ref="chartContent"></div>
+  </div>
+</template>
+
+<script>
+import echarts from "echarts";
+export default {
+    props: ["barColor", "progress", "city"],
+  name: "TEMPLATE",
+  data() {
+    return {
+      chartData: "71.23",
+    };
+  },
+  mounted() {
+    this.showChart();
+  },
+  methods: {
+    showChart() {
+      let chart = echarts.init(this.$refs.chartContent);
+      var data = {
+        value: this.chartData,
+        company: "年度能耗",
+        // ringColor: [{
+        //   offset: 0,
+        //   color: '#02d6fc' // 0% 处的颜色
+        // }, {
+        //   offset: 1,
+        //   color: '#367bec' // 100% 处的颜色
+        // }]
+
+        ringColor: [
+          {
+            offset: 0,
+            color:this.barColor, // 0% 处的颜色
+          },
+        ],
+      };
+      let option = {
+        title: {
+          text: data.value + "%\n" + data.company,
+          x: "center",
+          y: "center",
+          textStyle: {
+            color: "#fff",
+            fontSize: "18",
+          },
+        },
+        color: ["#1A273D"],
+        legend: {
+          show: false,
+          data: [],
+        },
+
+        series: [
+          {
+            name: "Line 1",
+            type: "pie",
+            clockWise: true,
+            //radius: ['55', '65'],//设置圆环的半径
+            radius: ["55", "65"], //设置圆环的半径
+            itemStyle: {
+              normal: {
+                label: {
+                  show: false,
+                },
+                labelLine: {
+                  show: false,
+                },
+              },
+            },
+            hoverAnimation: false,
+            data: [
+              {
+                value: data.value,
+                name: "",
+                itemStyle: {
+                  normal: {
+                    color: {
+                      // 完成的圆环的颜色
+                      colorStops: data.ringColor,
+                    },
+                    label: {
+                      show: false,
+                    },
+                    labelLine: {
+                      show: false,
+                    },
+                  },
+                },
+              },
+              {
+                name: "",
+                value: 100 - data.value,
+              },
+            ],
+          },
+        ],
+      };
+      chart.clear();
+      chart.setOption(option);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/mall/modules/PayMallDeal/index.vue
+++ src/views/mall/modules/PayMallDeal/index.vue
@@ -40,8 +40,8 @@
 <script>
 import gridtitle from "../../components/title/index";
 import vpay from "../../components/pay/index";
-// import pchart from "../../components/v-chart/persentchart/index";
-import pchart from "../../components/v-chart/canvaspersent/index";
+// import pchart from "../../components/v-chart/canvaspersent/index";
+import pchart from "../../components/v-chart/pie/index";
 import { mapGetters, mapActions, mapState } from "vuex";
 
 export default {
