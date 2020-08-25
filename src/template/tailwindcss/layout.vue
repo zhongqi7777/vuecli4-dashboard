62b7bdf6bf14dae3c682cfa1fea7e4b7860d7eaa
zhongqi7777 <yanlibo@300.cn>
2020-08-25 12:53:52 +0800

    modified:   src/template/tailwindcss/layout.vue

--- src/template/tailwindcss/layout.vue
+++ src/template/tailwindcss/layout.vue
@@ -95,25 +95,219 @@
     </div>
 
     <p class="text-2xl ...">
-      Inline 
+      Inline
     </p>
     <p class="text-lg mb-4">
       Use .inline to create an inline block-level element.
     </p>
 
     <div class="bg-gray-200 p-4">
-      <span
-        class="inline text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"
+      <span class="inline text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"
         >1</span
       >
-      <span
-        class="inline text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"
+      <span class="inline text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"
         >2</span
       >
-      <span
-        class="inline text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"
+      <span class="inline text-gray-700 text-center bg-gray-400 px-4 py-2 m-2"
         >3</span
       >
+    </div>
+
+    <p class="text-3xl mt-6 ...">Flex</p>
+    <p class="text-2xl ...">Row</p>
+    <p class="text-lg mb-4">
+      Use .flex-row to position flex items horizontally in the same direction as
+      text:
+    </p>
+
+    <div class="flex flex-row bg-gray-200">
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    <p class="text-2xl ...">Row Reversed</p>
+    <p class="text-lg mb-4">
+      Use .flex-row-reverse to position flex items horizontally in the opposite
+      direction:
+    </p>
+
+    <div class="flex flex-row-reverse bg-gray-200">
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    <p class="text-2xl ...">Column</p>
+    <p class="text-lg mb-4">
+      Use .flex-col to position flex items vertically:
+    </p>
+
+    <div class="flex flex-col bg-gray-200">
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    <p class="text-2xl ...">Column Reversed</p>
+    <p class="text-lg mb-4">
+      Use .flex-col-reverse to position flex items horizontally in the opposite
+      direction:
+    </p>
+
+    <div class="flex flex-col-reverse bg-gray-200">
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+    </div>
+
+    <p class="text-3xl mt-6 ...">Flex Wrap</p>
+    <p class="text-2xl ...">Don't wrap</p>
+    <p class="text-lg mb-4">
+      Use .flex-no-wrap to prevent flex items from wrapping, causing inflexible
+      items to overflow the container if necessary:
+    </p>
+
+    <div class="flex flex-no-wrap bg-gray-200">
+      <div class="w-2/5 flex-none p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">1</div>
+      </div>
+      <div class="w-2/5 flex-none p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">2</div>
+      </div>
+      <div class="w-2/5 flex-none p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">3</div>
+      </div>
+    </div>
+
+    <p class="text-2xl ...">Wrap normally</p>
+    <p class="text-lg mb-4">
+      Use .flex-wrap to allow flex items to wrap:
+    </p>
+
+    <div class="flex flex-wrap bg-gray-200">
+      <div class="w-2/5 p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">1</div>
+      </div>
+      <div class="w-2/5 p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">2</div>
+      </div>
+      <div class="w-2/5 p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">3</div>
+      </div>
+    </div>
+
+    <p class="text-2xl ...">Wrap reserved</p>
+    <p class="text-lg mb-4">
+      Use .flex-wrap to allow flex items to wrap:
+    </p>
+
+    <div class="flex flex-wrap-reverse bg-gray-200">
+      <div class="w-2/5 p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">1</div>
+      </div>
+      <div class="w-2/5 p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">2</div>
+      </div>
+      <div class="w-2/5 p-2">
+        <div class="text-gray-700 text-center bg-gray-400 p-2">3</div>
+      </div>
+    </div>
+
+    <p class="text-3xl mt-6 ...">Aligin Items</p>
+    <p class="text-2xl ...">stretch</p>
+    <p class="text-lg mb-4">
+      Use .items-stretch to stretch items to fill the flex container's cross
+      axis:
+    </p>
+
+    <div class="flex items-stretch bg-gray-200 h-24">
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        1
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        2
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        3
+      </div>
+    </div>
+
+    <p class="text-2xl ...">start</p>
+    <p class="text-lg mb-4">
+      Use .items-start to align items to the start of the flex container's cross
+      axis:
+    </p>
+
+    <div class="flex items-start bg-gray-200 h-24">
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        1
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        2
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        3
+      </div>
+    </div>
+
+    <p class="text-2xl ...">center</p>
+    <p class="text-lg mb-4">
+      Use .items-center to align items along the center of the flex container's
+      cross axis:
+    </p>
+
+    <div class="flex items-center bg-gray-200 h-24">
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        1
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        2
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        3
+      </div>
+    </div>
+
+    <p class="text-2xl ...">end</p>
+    <p class="text-lg mb-4">
+      Use .items-end to align items along the center of the flex container's
+      cross axis:
+    </p>
+
+    <div class="flex items-end bg-gray-200 h-24">
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        1
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        2
+      </div>
+      <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+        3
+      </div>
+    </div>
+    <p class="text-2xl ...">baseline</p>
+    <p class="text-lg mb-4">
+      Use .items-baseline to align items along the center of the flex
+      container's cross axis:
+    </p>
+
+    <div class="flex items-baseline bg-gray-200 h-24">
+      <div
+        class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 text-base"
+      >
+        1
+      </div>
+      <div
+        class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 text-2xl"
+      >
+        2
+      </div>
+      <div
+        class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2 text-lg"
+      >
+        3
+      </div>
     </div>
   </div>
 </template>
